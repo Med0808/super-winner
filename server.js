fc07421955cdcf145176b79351213b26fccbded0
Med Amine <56002560+Med0808@users.noreply.github.com>
2025-10-20 04:27:49 +0100

    first commit1Z

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.post("/create-payment-intent", async (req, res) => {
   }
 });
 
-app.all("/*", (req, res) => res.status(404).json({ error: "Not Found" }));
+app.use((req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
 
 
 export default app;
